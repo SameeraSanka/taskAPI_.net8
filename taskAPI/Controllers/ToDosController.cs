4abb38fb1875de23f782dc9068e3d706b034034b
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-03-21 13:13:15 +0530

    new changes

---
+++ taskAPI/Controllers/ToDosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace taskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetToDos() 
+        { 
+            return Ok();
+        }
+    }
+}
---
+++ taskAPI/Models/Todo.cs
@@ -0,0 +1,7 @@
+﻿ namespace taskAPI.Models
+{
+    public class Todo
+    {
+         public int Id { get; set; }
+    }
+}
